e354394e60e465689240cd6546c81a91292cb42a
bytecoder8 <58306822+bytecoder8@users.noreply.github.com>
2019-09-07 12:05:05 +0500

    update slider

--- js/index.js
+++ js/index.js
@@ -11,4 +11,32 @@
 
   $contactShowButton.addEventListener('click', hideShow);
   $closeButton.addEventListener('click', hideShow);
+
+  // slider
+  var $sliderButtons = document.querySelectorAll('.slider__button');
+
+  for (var i = 0; i < $sliderButtons.length; i++) {
+    $sliderButtons[i].addEventListener('click', function (e) {
+      var num = e.currentTarget.dataset.slider;
+
+      hideElements('.slider__element', 'slider__element--hidden');
+      hideElements('.slider__card', 'slider__card--hidden');
+
+      document.querySelector('.slider__element[data-slider="'+num+'"]').classList.remove('slider__element--hidden');
+      document.querySelector('.slider__card[data-slider="'+num+'"]').classList.remove('slider__card--hidden');
+    });
+  }
+
+  function hideElements(selector, cls) {
+    var $elements = document.querySelectorAll(selector);
+    for (var i = 0; i < $elements.length; i++) {
+      $elements[i].classList.add(cls);
+    }
+  }
+  function showElements(selector, cls) {
+    var $elements = document.querySelectorAll(selector);
+    for (var i = 0; i < $elements.length; i++) {
+      $elements[i].classList.remove(cls);
+    }
+  }
 })()
